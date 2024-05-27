f345fa3542be5ecb55dfb51eb983d1a668dfbcc8
KimDoubleB <tree9295@gmail.com>
2024-05-28 01:25:30 +0900

    feat: read property using yaml property factory

--- piikii-bootstrap/src/main/kotlin/com/piikii/bootstrap/BootstrapApplication.kt
+++ piikii-bootstrap/src/main/kotlin/com/piikii/bootstrap/BootstrapApplication.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.runApplication
 class BootstrapApplication
 
 fun main(args: Array<String>) {
-     System.setProperty("spring.config.location", "classpath:/database-config/,classpath:/cache-config/,classpath:/broker-config/");
     runApplication<BootstrapApplication>(*args)
 }
