191088bf51b226fc86cecd7f8711678ff89ccb20
Diger <kitten.diger@gmail.com>
2024-06-03 23:03:10 +0900

    style : Delete Unnecessary Code

--- piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/user/UserJpaRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.piikii.output.persistence.postgresql.user
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserJpaRepository : JpaRepository<UserEntity, Long>
