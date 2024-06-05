3cd89036c17cb0294f3986dec1b869989eb76b44
Diger <kitten.diger@gmail.com>
2024-06-05 22:02:43 +0900

    refact : Persistence Adapter Integrate

--- piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/room/RoomQueryAdapter.kt
+++ piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/room/RoomQueryAdapter.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
-@Transactional(readOnly = true)
+
 class RoomQueryAdapter(
     private val roomRepository: RoomRepository
 ) : RoomQueryPort {
--- piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/sourceplace/SourcePlaceAdapter.kt
+++ piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/sourceplace/SourcePlaceAdapter.kt
@@ -2,6 +2,7 @@ package com.piikii.output.persistence.postgresql.adapter.sourceplace
 
 import com.piikii.application.domain.model.sourceplace.SourcePlace
 import com.piikii.application.port.output.persistence.SourcePlaceCommandPort
+import com.piikii.application.port.output.persistence.SourcePlaceQueryPort
 import com.piikii.output.persistence.postgresql.persistence.entity.SourcePlaceEntity
 import com.piikii.output.persistence.postgresql.persistence.entity.SourcePlaceEntity.Companion.toVO
 import com.piikii.output.persistence.postgresql.persistence.repository.SourcePlaceRepository
@@ -9,22 +10,33 @@ import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
-@Transactional
-class SourcePlaceCommandAdapter(
+@Transactional(readOnly = true)
+class SourcePlaceAdapter(
     private val sourcePlaceRepository: SourcePlaceRepository
-) : SourcePlaceCommandPort {
+) : SourcePlaceCommandPort, SourcePlaceQueryPort {
 
+    @Transactional
     override fun save(sourcePlace: SourcePlace): SourcePlace {
         val sourcePlaceEntity = SourcePlaceEntity.toEntity(sourcePlace)
         sourcePlaceRepository.save(sourcePlaceEntity)
         return sourcePlaceEntity.toVO(sourcePlaceEntity)
     }
 
+    @Transactional
     override fun update(sourcePlace: SourcePlace, id: Long) {
         TODO("Not yet implemented")
     }
 
+    @Transactional
     override fun delete(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieve(id: Long): SourcePlace {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieveAll(ids: List<Long>): List<SourcePlace> {
         TODO("Not yet implemented")
     }
 }
