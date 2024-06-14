e789ff0c8c4e482f76adeddbafd599ab9fc43591
thguss <thgus345@naver.com>
2024-06-15 02:51:17 +0900

    feat : implement api of room

--- piikii-application/src/main/kotlin/com/piikii/application/port/input/RoomUseCase.kt
+++ piikii-application/src/main/kotlin/com/piikii/application/port/input/RoomUseCase.kt
@@ -3,5 +3,8 @@ package com.piikii.application.port.input
 import com.piikii.application.domain.room.Room
 
 interface RoomUseCase {
-    fun save(loginId: String): Room?
+    fun save(request: Room): Room
+    fun update(roomId: Long, request: Room)
+    fun delete(roomId: Long)
+    fun retrieve(roomId: Long): Room
 }
