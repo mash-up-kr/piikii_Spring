ffdb371cdb32e12bfd1b70dfd912fbdd5385d44f
thguss <thgus345@naver.com>
2024-06-16 16:39:21 +0900

    fix : review reflection - fix response type of api

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/controller/room/RoomApi.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/controller/room/RoomApi.kt
@@ -6,7 +6,7 @@ import com.piikii.application.port.input.room.dto.response.RoomSaveResponseForm
 import com.piikii.input.http.controller.room.dto.request.RoomSaveRequestForm
 import com.piikii.input.http.controller.room.dto.request.RoomUpdateRequestForm
 import com.piikii.input.http.generic.ResponseForm
-import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,62 +14,58 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/rooms")
 class RoomApi(
     private val roomUseCase: RoomUseCase,
 ) {
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun saveRoom(
         @RequestBody request: RoomSaveRequestForm,
-    ): ResponseEntity<ResponseForm<RoomSaveResponseForm>> {
+    ): ResponseForm<RoomSaveResponseForm> {
         val response = roomUseCase.save(request.toDomain())
-        return ResponseEntity.created(URI("/")).body(
-            ResponseForm(
-                data = response,
-                message = "방 생성 성공",
-            ),
+        return ResponseForm(
+            data = response,
+            message = "방 생성 성공",
         )
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{roomId}")
     fun updateRoom(
         @RequestBody request: RoomUpdateRequestForm,
         @PathVariable roomId: Long,
-    ): ResponseEntity<ResponseForm<Any>> {
+    ): ResponseForm<Any> {
         roomUseCase.update(request.toDomain(), roomId)
-        return ResponseEntity.ok(
-            ResponseForm(
-                message = "방 정보 수정 성공",
-            ),
+        return ResponseForm(
+            message = "방 정보 수정 성공",
         )
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{roomId}")
     fun deleteRoom(
         @PathVariable roomId: Long,
-    ): ResponseEntity<ResponseForm<Any>> {
+    ): ResponseForm<Any> {
         roomUseCase.delete(roomId)
-        return ResponseEntity.ok(
-            ResponseForm(
-                message = "방 삭제 성공",
-            ),
+        return ResponseForm(
+            message = "방 삭제 성공",
         )
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{roomId}")
     fun getRoom(
         @PathVariable roomId: Long,
-    ): ResponseEntity<ResponseForm<RoomGetResponseForm>> {
+    ): ResponseForm<RoomGetResponseForm> {
         val response = roomUseCase.retrieve(roomId)
-        return ResponseEntity.ok(
-            ResponseForm(
-                data = response,
-                message = "방 조회 성공",
-            ),
+        return ResponseForm(
+            data = response,
+            message = "방 조회 성공",
         )
     }
 }
