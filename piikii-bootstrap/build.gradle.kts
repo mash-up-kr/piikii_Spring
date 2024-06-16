fa4b15c904cec6762adf10469ed2320b46b474db
KimDoubleB <tree9295@gmail.com>
2024-06-16 18:21:52 +0900

    chore: added spring web dependency to bootstrap

--- piikii-bootstrap/build.gradle.kts
+++ piikii-bootstrap/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
     implementation(project(":piikii-output-persistence:postgresql"))
     implementation(project(":piikii-output-cache:redis"))
     implementation(project(":piikii-output-cache:caffeine"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
