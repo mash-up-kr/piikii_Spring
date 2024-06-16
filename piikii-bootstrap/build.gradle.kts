f16b0c6e02068b09f07b784969a008f9c323f0a2
Diger <kitten.diger@gmail.com>
2024-06-16 17:33:35 +0900

    chore : Change Method For Generate Image

--- piikii-bootstrap/build.gradle.kts
+++ piikii-bootstrap/build.gradle.kts
@@ -7,3 +7,16 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+    imageName.set("mashupmz/piikii")
+    environment.set(mapOf("BP_JVM_VERSION" to "21"))
+    docker {
+        publishRegistry {
+            username.set(System.getenv("DOCKER_HUB_USERNAME"))
+            password.set(System.getenv("DOCKER_HUB_PASSWORD"))
+            url.set(System.getenv("DOCKER_HUB_REPOSITORY"))
+        }
+    }
+    publish.set(true)
+}
