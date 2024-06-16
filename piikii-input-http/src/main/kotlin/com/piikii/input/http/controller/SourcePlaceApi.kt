701c05f6e8672b0fe034dd9b392299f51a1c4c13
KimDoubleB <tree9295@gmail.com>
2024-06-16 15:21:36 +0900

    feat: apply example documentation interface to controller class

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/controller/SourcePlaceApi.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/controller/SourcePlaceApi.kt
@@ -2,21 +2,22 @@ package com.piikii.input.http.controller
 
 import com.piikii.application.domain.sourceplace.SourcePlace
 import com.piikii.application.port.input.SourcePlaceUseCase
+import com.piikii.input.http.docs.SourcePlaceApiDocs
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SourcePlaceApi(
-    private val sourcePlaceUseCase: SourcePlaceUseCase,
-) {
-    @PostMapping
-    fun testPost(): SourcePlace {
+    private val sourcePlaceUseCase: SourcePlaceUseCase
+) : SourcePlaceApiDocs {
+
+    override fun testPost(): SourcePlace {
         return sourcePlaceUseCase.save()
     }
 
-    @GetMapping
-    fun testGet(): SourcePlace {
+    override fun testGet(): SourcePlace {
         return sourcePlaceUseCase.retrieve()
     }
+
 }
