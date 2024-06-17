bb54e59d46a0f9483195146c78ceda7b471142e7
Diger <kitten.diger@gmail.com>
2024-06-18 00:57:49 +0900

    feat: Add Exception Logging Interceptor

---
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/interceptor/LoggingInterceptor.kt
@@ -0,0 +1,95 @@
+package com.piikii.input.http.interceptor
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.piikii.common.logutil.HookLogger
+import com.piikii.common.logutil.SystemLogger.logger
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.util.ContentCachingRequestWrapper
+import org.springframework.web.util.ContentCachingResponseWrapper
+
+@Component
+class LoggingInterceptor(
+    private val hookLogger: HookLogger,
+    private val objectMapper: ObjectMapper,
+) : HandlerInterceptor {
+    override fun preHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+    ): Boolean {
+        if (request !is ContentCachingRequestWrapper) {
+            val wrappedRequest = ContentCachingRequestWrapper(request)
+            request.setAttribute("wrappedRequest", wrappedRequest)
+        }
+
+        if (response !is ContentCachingResponseWrapper) {
+            val wrappedResponse = ContentCachingResponseWrapper(response)
+            request.setAttribute("wrappedResponse", wrappedResponse)
+        }
+        return true
+    }
+
+    override fun postHandle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        modelAndView: ModelAndView?,
+    ) {
+    }
+
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?,
+    ) {
+        val wrappedResponse = response as ContentCachingResponseWrapper
+        val responseBody = wrappedResponse.contentAsByteArray.toString(Charsets.UTF_8)
+        val errorResponse = parseErrorResponse(responseBody)
+
+        val message = errorResponse?.get("message")?.asText().orEmpty()
+        val cause = errorResponse?.get("cause")?.asText().orEmpty()
+
+        val logMessage = createLogMessage(request, message, cause)
+        logResponse(wrappedResponse.status, logMessage)
+        wrappedResponse.copyBodyToResponse()
+    }
+
+    private fun parseErrorResponse(responseBody: String) =
+        runCatching {
+            objectMapper.readTree(responseBody)
+        }.getOrNull()
+
+    private fun createLogMessage(
+        request: HttpServletRequest,
+        message: String,
+        cause: String,
+    ): String {
+        val logNode =
+            objectMapper.createObjectNode().apply {
+                put("uri", request.requestURI)
+                put("method", request.method)
+                put("queryString", request.queryString)
+                put("message", message)
+                put("cause", cause)
+            }
+        return objectMapper.writeValueAsString(logNode)
+    }
+
+    private fun logResponse(
+        status: Int,
+        logMessage: String,
+    ) {
+        when (status) {
+            in 400..499 -> logger.warn(logMessage)
+            500 -> {
+                logger.error(logMessage)
+                hookLogger.send(logMessage)
+            }
+        }
+    }
+}
