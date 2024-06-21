e7c164361d1b887819df0049e0e1868e3aa729c2
thguss <thgus345@naver.com>
2024-06-22 00:52:36 +0900

    add: add a part of voting method

---
+++ piikii-application/src/main/kotlin/com/piikii/application/domain/roomvote/RoomVoteService.kt
@@ -0,0 +1,29 @@
+package com.piikii.application.domain.roomvote
+
+import com.piikii.application.port.output.persistence.RoomQueryPort
+import com.piikii.common.exception.ExceptionCode
+import com.piikii.common.exception.PiikiiException
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.UUID
+
+@Service
+class RoomVoteService(
+    private val roomQueryPort: RoomQueryPort,
+) {
+    fun vote(roomId: UUID) {
+        val room = roomQueryPort.retrieve(roomId)
+        if (room.voteDeadline.isBefore(LocalDateTime.now())) {
+            throw PiikiiException(
+                exceptionCode = ExceptionCode.ACCESS_DENIED,
+                detailMessage = VOTE_IS_CLOSED,
+            )
+        }
+
+        TODO("implementation additional flow")
+    }
+
+    companion object {
+        const val VOTE_IS_CLOSED = "투표가 마감되었습니다."
+    }
+}
