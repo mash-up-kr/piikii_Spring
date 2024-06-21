d32906456897b07de6019c3e1fbbbdb65a8fa22d
Diger <kitten.diger@gmail.com>
2024-06-22 00:10:27 +0900

    chore: Diet Dependency For RestClient

--- piikii-common/build.gradle.kts
+++ piikii-common/build.gradle.kts
@@ -1,7 +1,12 @@
 project(":piikii-common") {
     // for RestClient
     dependencies {
-        implementation("org.springframework:spring-web")
+        implementation("org.springframework:spring-web") {
+            exclude("org.springframework:spring-beans")
+            exclude("org.springframework:spring-core")
+            exclude("org.springframework:spring-aop")
+            exclude("org.springframework:spring-context")
+        }
     }
 }
 
