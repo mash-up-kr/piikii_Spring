b60aeda9906337e9870dc30e29580886bad0896d
Diger <kitten.diger@gmail.com>
2024-06-24 23:19:04 +0900

    chore: Speed Up Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,43 +2,52 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    alias(libs.plugins.springBoot)
-    alias(libs.plugins.springDependencyManagement)
-    alias(libs.plugins.kotlin)
-    alias(libs.plugins.kotlinSpring)
-    alias(libs.plugins.kotlinJpa)
-    alias(libs.plugins.kotlinKapt)
-    alias(libs.plugins.ktLint)
-    application
+    id(Plugins.SPRING_BOOT) version Versions.SPRING_BOOT
+    id(Plugins.SPRING_DEPENDENCY_MANAGEMENT) version Versions.SPRING_DEPENDENCY_MANAGEMENT
+    id(Plugins.APPLICATION)
+    id(Plugins.KT_LINT) version Versions.KT_LINT_VERSION
+
+    kotlin(Plugins.JVM) version Versions.KOTLIN
+    kotlin(Plugins.SPRING) version Versions.KOTLIN
+    kotlin(Plugins.JPA) version Versions.JPA_PLUGIN
+    kotlin(Plugins.KAPT) version Versions.KAPT_PLUGIN
 }
 
 allprojects {
     group = "com.piikii"
     version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.isIncremental = true
+        options.isFork = true
+    }
 }
 
 subprojects {
-    apply {
-        plugin("org.jetbrains.kotlin.jvm")
-        plugin("org.jetbrains.kotlin.plugin.spring")
-        plugin("org.springframework.boot")
-        plugin("kotlin")
-        plugin("java-library")
-        plugin("kotlin-jpa")
-        plugin("io.spring.dependency-management")
-        plugin("kotlin-kapt")
-        plugin("application")
-        plugin("org.jlleitschuh.gradle.ktlint")
-    }
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "kotlin")
+    apply(plugin = "java-library")
+    apply(plugin = "kotlin-jpa")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "kotlin-kapt")
+    apply(plugin = "application")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     dependencies {
+        // for kotlin
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+        // for spring commons
         implementation("org.springframework.boot:spring-boot-autoconfigure")
         implementation("org.springframework.boot:spring-boot-starter-logging")
-        implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-        implementation("ch.qos.logback:logback-classic")
-
+        // for test
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
@@ -66,6 +75,13 @@ subprojects {
 configure(allprojects.filter { it.name != "piikii-common" }) {
     dependencies {
         implementation(project(":piikii-common"))
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheDynamicVersionsFor(10 * 60, TimeUnit.SECONDS)
+        cacheChangingModulesFor(4, TimeUnit.HOURS)
     }
 }
 
