1b6e453b6bd36ac566afcd57d8d25cacbe25c635
Diger <kitten.diger@gmail.com>
2024-06-28 23:54:47 +0900

    fix: Resolve Merge Conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,14 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id(Plugins.SPRING_BOOT) version Versions.SPRING_BOOT
-    id(Plugins.SPRING_DEPENDENCY_MANAGEMENT) version Versions.SPRING_DEPENDENCY_MANAGEMENT
-    id(Plugins.APPLICATION)
-    id(Plugins.KT_LINT) version Versions.KT_LINT_VERSION
-
-    kotlin(Plugins.JVM) version Versions.KOTLIN
-    kotlin(Plugins.SPRING) version Versions.KOTLIN
-    kotlin(Plugins.JPA) version Versions.JPA_PLUGIN
-    kotlin(Plugins.KAPT) version Versions.KAPT_PLUGIN
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlinSpring)
+    alias(libs.plugins.kotlinJpa)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.ktLint)
+    application
 }
 
 allprojects {
@@ -28,26 +27,27 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "kotlin")
-    apply(plugin = "java-library")
-    apply(plugin = "kotlin-jpa")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "kotlin-kapt")
-    apply(plugin = "application")
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("org.springframework.boot")
+        plugin("kotlin")
+        plugin("java-library")
+        plugin("kotlin-jpa")
+        plugin("io.spring.dependency-management")
+        plugin("kotlin-kapt")
+        plugin("application")
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
 
     dependencies {
-        // for kotlin
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-        // for spring commons
         implementation("org.springframework.boot:spring-boot-autoconfigure")
         implementation("org.springframework.boot:spring-boot-starter-logging")
-        // for test
+        implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+        implementation("ch.qos.logback:logback-classic")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
@@ -78,15 +78,15 @@ configure(allprojects.filter { it.name != "piikii-common" }) {
     }
 }
 
+java.toolchain.languageVersion = JavaLanguageVersion.of(libs.versions.javaVersion.get())
+
+tasks.bootJar {
+    enabled = false
+}
+
 configurations.all {
     resolutionStrategy {
         cacheDynamicVersionsFor(10 * 60, TimeUnit.SECONDS)
         cacheChangingModulesFor(4, TimeUnit.HOURS)
     }
-}
-
-java.toolchain.languageVersion = JavaLanguageVersion.of(libs.versions.javaVersion.get())
-
-tasks.bootJar {
-    enabled = false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,5 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-    }
 }
 
 rootProject.name = "piikii"
