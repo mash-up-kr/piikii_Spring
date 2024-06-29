ddd0f633c88633bdf2c401c35f469da14105941b
thguss <thgus345@naver.com>
2024-06-30 03:45:41 +0900

    delete: delete persistence of Course

--- piikii-application/src/main/kotlin/com/piikii/application/domain/course/Course.kt
+++
@@ -1,7 +0,0 @@
-package com.piikii.application.domain.course
-
-class Course(
-    val roomId: Long,
-    val scheduleId: Long,
-    val placeId: Long,
-)
--- piikii-application/src/main/kotlin/com/piikii/application/port/output/persistence/CoursePort.kt
+++
@@ -1,9 +0,0 @@
-package com.piikii.application.port.output.persistence
-
-import java.util.UUID
-
-interface CourseQueryPort {
-    fun isCourseExist(roomId: UUID): Boolean
-}
-
-interface CourseCommandPort
