bfbc0b01ed7ef92863a91146fdfa91f8eacdf3e6
KimDoubleB <tree9295@gmail.com>
2024-06-29 23:13:22 +0900

    feat: split vote unavailable - finish

--- piikii-application/src/main/kotlin/com/piikii/application/domain/vote/VoteService.kt
+++ piikii-application/src/main/kotlin/com/piikii/application/domain/vote/VoteService.kt
@@ -6,6 +6,7 @@ import com.piikii.application.port.output.persistence.VoteCommandPort
 import com.piikii.common.exception.ExceptionCode
 import com.piikii.common.exception.PiikiiException
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Service
@@ -17,10 +18,10 @@ class VoteService(
         roomId: UUID,
         votes: List<Vote>,
     ) {
-        if (roomQueryPort.retrieve(roomId).isUnavailableToVote()) {
+        if (roomQueryPort.retrieve(roomId).isVoteUnavailable()) {
             throw PiikiiException(
                 exceptionCode = ExceptionCode.ACCESS_DENIED,
-                detailMessage = VOTE_ACCESS_DENIED,
+                detailMessage = VOTE_UNAVAILABLE,
             )
         }
         // TODO: votes.map { it.roomPlaceId } 존재여부 검증 필요 -> 도현이 작업 완료되면 붙일 예정
@@ -28,10 +29,17 @@ class VoteService(
     }
 
     override fun isVoteFinished(roomId: UUID): Boolean {
-        return !roomQueryPort.retrieve(roomId).isUnavailableToVote()
+        val voteDeadline =
+            roomQueryPort.retrieve(roomId).voteDeadline
+                ?: throw PiikiiException(
+                    exceptionCode = ExceptionCode.ACCESS_DENIED,
+                    detailMessage = VOTE_NOT_STARTED,
+                )
+        return voteDeadline.isBefore(LocalDateTime.now())
     }
 
     companion object {
-        const val VOTE_ACCESS_DENIED = "투표가 시작되지 않았거나, 마감되었습니다."
+        const val VOTE_UNAVAILABLE = "투표가 시작되지 않았거나, 마감되었습니다"
+        const val VOTE_NOT_STARTED = "투표가 시작되지 않았습니다"
     }
 }
