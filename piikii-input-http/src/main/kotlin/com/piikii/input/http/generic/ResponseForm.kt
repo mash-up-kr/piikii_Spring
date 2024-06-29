a71b10d965ca9a256e3dcc713a3387256dda1972
KimDoubleB <tree9295@gmail.com>
2024-06-29 15:30:16 +0900

    refactor: replace `@JsonInclude(JsonInclude.Include.NON_NULL)` to property

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/generic/ResponseForm.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/generic/ResponseForm.kt
@@ -1,8 +1,5 @@
 package com.piikii.input.http.generic
 
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 class ResponseForm<T>(
     val data: T? = null,
     val message: String? = null,
