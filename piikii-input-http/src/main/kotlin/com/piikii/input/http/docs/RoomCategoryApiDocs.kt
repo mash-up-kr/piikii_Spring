2a4099950287be4804d8331574eb93ceed696138
thguss <thgus345@naver.com>
2024-06-29 21:51:12 +0900

    add: add detail element of docs

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/docs/RoomCategoryApiDocs.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/docs/RoomCategoryApiDocs.kt
@@ -7,6 +7,8 @@ import com.piikii.input.http.generic.ResponseForm
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
@@ -18,20 +20,51 @@ interface RoomCategoryApiDocs {
     @Operation(summary = "방 카테고리 생성 api", description = "방 카테고리를 생성합니다.")
     @ApiResponses(value = [ApiResponse(responseCode = "201", description = "CREATED success")])
     fun createRoomCategories(
-        @Parameter(name = "roomId", description = "카테고리를 생성할 방 uuid", `in` = ParameterIn.PATH) roomId: UUID,
-        @RequestBody request: RoomCategoriesCreateRequest,
+        @Parameter(
+            name = "roomId",
+            description = "카테고리를 생성할 방 uuid",
+            required = true,
+            `in` = ParameterIn.PATH,
+        ) roomId: UUID,
+        @RequestBody(
+            description = "카테고리 생성 Request Body",
+            required = true,
+            content = [Content(schema = Schema(implementation = RoomCategoriesCreateRequest::class))],
+        ) request: RoomCategoriesCreateRequest,
     ): ResponseForm<Unit>
 
     @Operation(summary = "방 카테고리 조회 api", description = "방 카테고리를 조회합니다.")
-    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "OK success")])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "OK success",
+                content = [Content(schema = Schema(implementation = RoomCategoriesReadResponse::class))],
+            ),
+        ],
+    )
     fun readRoomCategories(
-        @Parameter(name = "roomId", description = "카테고리를 조회할 방 uuid", `in` = ParameterIn.PATH) roomId: UUID,
+        @Parameter(
+            name = "roomId",
+            description = "카테고리를 조회할 방 uuid",
+            required = true,
+            `in` = ParameterIn.PATH,
+        ) roomId: UUID,
     ): ResponseForm<RoomCategoriesReadResponse>
 
     @Operation(summary = "방 카테고리 삭제 api", description = "방 카테고리를 삭제합니다.")
     @ApiResponses(value = [ApiResponse(responseCode = "204", description = "NO_CONTENT success")])
     fun deleteRoomCategories(
-        @Parameter(name = "roomId", description = "카테고리를 삭제할 방 uuid", `in` = ParameterIn.PATH) roomId: UUID,
-        @RequestBody request: RoomCategoryIdsRequest,
+        @Parameter(
+            name = "roomId",
+            description = "카테고리를 삭제할 방 uuid",
+            required = true,
+            `in` = ParameterIn.PATH,
+        ) roomId: UUID,
+        @RequestBody(
+            description = "카테고리 삭제 Request Body",
+            required = true,
+            content = [Content(schema = Schema(implementation = RoomCategoryIdsRequest::class))],
+        ) request: RoomCategoryIdsRequest,
     ): ResponseForm<Unit>
 }
