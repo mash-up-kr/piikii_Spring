72a88557e1e10f25b967b10f0092178564bd59a5
KimDoubleB <tree9295@gmail.com>
2024-06-29 20:32:35 +0900

    feat: added Enumerated to VoteResult

--- piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/persistence/entity/VoteEntity.kt
+++ piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/persistence/entity/VoteEntity.kt
@@ -5,6 +5,8 @@ import com.piikii.application.domain.vote.VoteResult
 import com.piikii.output.persistence.postgresql.persistence.common.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Table
 import org.hibernate.annotations.DynamicUpdate
 import org.hibernate.annotations.SQLRestriction
@@ -19,6 +21,7 @@ class VoteEntity(
     val userId: UUID,
     @Column(name = "room_place_id", nullable = false)
     val roomPlaceId: Long,
+    @Enumerated(EnumType.STRING)
     @Column(name = "result", nullable = false, length = 10)
     val result: VoteResult,
 ) : BaseEntity() {
