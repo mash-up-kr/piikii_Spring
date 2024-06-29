d3d5d92807bd267cff175fd1e356a7a887510710
KimDoubleB <tree9295@gmail.com>
2024-06-29 23:31:13 +0900

    feat: added ROOM_PASSWORD_INVALID Exception code

--- piikii-common/src/main/kotlin/com/piikii/common/exception/ExceptionCode.kt
+++ piikii-common/src/main/kotlin/com/piikii/common/exception/ExceptionCode.kt
@@ -7,6 +7,7 @@ enum class ExceptionCode(
     // 400
     ILLEGAL_ARGUMENT_EXCEPTION(400, "요청 값이 올바르지 않습니다."),
     UNAUTHORIZED(401, "인증된 토큰으로부터의 요청이 아닙니다."),
+    ROOM_PASSWORD_INVALID(401, "방 패스워드가 틀립니다."),
     ACCESS_DENIED(403, "해당 리소스에 접근할 수 없습니다."),
     NOT_FOUNDED(404, "해당 리소스를 찾을 수 없습니다."),
     CONFLICT(409, "해당 리소스가 중복됩니다."),
