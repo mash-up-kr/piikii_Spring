3b4cf79b780b625f769cd61741458e1f0db1ab50
thguss <thgus345@naver.com>
2024-06-30 03:48:23 +0900

    fix: apply added column of Room

--- piikii-application/src/main/kotlin/com/piikii/application/domain/course/CourseService.kt
+++ piikii-application/src/main/kotlin/com/piikii/application/domain/course/CourseService.kt
@@ -1,15 +1,20 @@
 package com.piikii.application.domain.course
 
 import com.piikii.application.port.input.CourseUseCase
-import com.piikii.application.port.output.persistence.CourseQueryPort
+import com.piikii.application.port.input.dto.response.CourseResponse
+import com.piikii.application.port.output.persistence.RoomQueryPort
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
 class CourseService(
-    private val courseQueryPort: CourseQueryPort,
+    private val roomQueryPort: RoomQueryPort
 ) : CourseUseCase {
     override fun isCourseExist(roomId: UUID): Boolean {
-        return courseQueryPort.isCourseExist(roomId)
+        return roomQueryPort.retrieve(roomId).isCourseCreated
+    }
+
+    override fun createCourse(roomId: UUID): CourseResponse {
+        TODO("Not yet implemented")
     }
 }
