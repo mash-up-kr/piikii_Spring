23142881a202101ec4ec1aead2ef13b1410652b5
KimDoubleB <tree9295@gmail.com>
2024-07-22 02:25:58 +0900

    feat: set integration test environment

---
+++ piikii-output-web/avocado/src/test/kotlin/com/piikii/output/web/avocado/url/ShareUrlParserTest.kt
@@ -0,0 +1,33 @@
+package com.piikii.output.web.avocado.url
+
+import com.piikii.output.web.avocado.TestConfiguration
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+
+@Disabled
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = [TestConfiguration::class])
+class ShareUrlParserTest {
+    @Autowired
+    lateinit var sharePlaceIdParser: SharePlaceIdParser
+
+    @Autowired
+    lateinit var mapPlaceIdParser: MapPlaceIdParser
+
+    @Test
+    fun sharePlaceIdParserTest() {
+        val parse = sharePlaceIdParser.parse("주소를 입력하세요")
+        println("parse = $parse")
+    }
+
+    @Test
+    fun mapPlaceIdParserTest() {
+        val parse = mapPlaceIdParser.parse("주소를 입력하세요")
+        println("parse = $parse")
+    }
+}
