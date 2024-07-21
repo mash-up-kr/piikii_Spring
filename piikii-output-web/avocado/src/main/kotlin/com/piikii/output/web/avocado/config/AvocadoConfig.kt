f56133d5edeb68077522d24c1693343417144273
KimDoubleB <tree9295@gmail.com>
2024-07-22 02:28:16 +0900

    feat: define avocado api client bean

--- piikii-output-web/avocado/src/main/kotlin/com/piikii/output/web/avocado/config/AvocadoConfig.kt
+++ piikii-output-web/avocado/src/main/kotlin/com/piikii/output/web/avocado/config/AvocadoConfig.kt
@@ -2,11 +2,40 @@ package com.piikii.output.web.avocado.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.web.client.RestClient
 
 @Configuration
 @EnableConfigurationProperties(AvocadoProperties::class)
-class AvocadoConfig
+class AvocadoConfig {
+    @Bean
+    fun avocadoApiClient(avocadoProperties: AvocadoProperties): RestClient {
+        val httpProperties = avocadoProperties.http
+        return RestClient.builder()
+            .baseUrl(avocadoProperties.url.api)
+            .defaultHeaders {
+                it.add(HttpHeaders.COOKIE, httpProperties.cookies)
+                it.add(HttpHeaders.USER_AGENT, httpProperties.userAgent)
+                it.add(HttpHeaders.ACCEPT_LANGUAGE, "ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4")
+                it.add(HttpHeaders.CACHE_CONTROL, "no-cache")
+                it.add(HttpHeaders.PRAGMA, "no-cache")
+
+                it.add("Priority", "u=1")
+                it.add("Dnt", "1")
+
+                it.add("Sec-Ch-Ua", "\"Not-A.Brand\";v=\"99\", \"Chromium\";v=\"124\"")
+                it.add("Sec-Ch-Ua-Mobile", "?0")
+                it.add("Sec-Ch-Ua-Platform", "MacOS")
+
+                it.add("Sec-Fetch-Dest", "empty")
+                it.add("Sec-Fetch-Mode", "cors")
+                it.add("Sec-Fetch-Site", "same-origin")
+            }
+            .build()
+    }
+}
 
 @ConfigurationProperties(prefix = "avocado")
 data class AvocadoProperties(
