e0a9da71df4606b928e0f4ea11a9b0cab6c5a7e1
KimDoubleB <tree9295@gmail.com>
2024-07-22 01:01:02 +0900

    feat: init avocado url parser

---
+++ piikii-output-web/avocado/src/main/kotlin/com/piikii/output/web/avocado/config/AvocadoConfig.kt
@@ -0,0 +1,32 @@
+package com.piikii.output.web.avocado.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(AvocadoProperties::class)
+class AvocadoConfig
+
+@ConfigurationProperties(prefix = "avocado")
+data class AvocadoProperties(
+    val url: AvocadoUrl,
+)
+
+data class AvocadoUrl(
+    val regex: Regex,
+    val api: String,
+) {
+    data class Regex(
+        val web: String,
+        val share: String,
+    )
+
+    fun webRegex(): kotlin.text.Regex {
+        return regex.web.toRegex()
+    }
+
+    fun shareRegex(): kotlin.text.Regex {
+        return regex.share.toRegex()
+    }
+}
