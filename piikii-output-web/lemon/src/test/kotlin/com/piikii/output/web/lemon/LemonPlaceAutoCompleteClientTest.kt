da412e85f32d9183e8d41958e48a4528c00d367c
KimDoubleB <tree9295@gmail.com>
2024-07-26 00:12:36 +0900

    feat: added lemon isAutoCompleteSupportedUrlTest

--- piikii-output-web/lemon/src/test/kotlin/com/piikii/output/web/lemon/LemonPlaceAutoCompleteClientTest.kt
+++ piikii-output-web/lemon/src/test/kotlin/com/piikii/output/web/lemon/LemonPlaceAutoCompleteClientTest.kt
@@ -1,6 +1,7 @@
 package com.piikii.output.web.lemon
 
 import com.piikii.output.web.lemon.adapter.LemonPlaceAutoCompleteClient
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,11 +11,18 @@ import org.springframework.test.context.ContextConfiguration
 
 @Disabled
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles(profiles = ["test", "local"])
 @ContextConfiguration(classes = [TestConfiguration::class])
 class LemonPlaceAutoCompleteClientTest {
     @Autowired
     lateinit var lemonPlaceAutoCompleteClient: LemonPlaceAutoCompleteClient
+
+    @Test
+    fun isAutoCompleteSupportedUrlTest() {
+        val url = "URL을 입력해주세요"
+
+        assertThat(lemonPlaceAutoCompleteClient.isAutoCompleteSupportedUrl(url)).isNotNull()
+    }
 
     @Test
     fun getAutoCompletedPlaceTest() {
