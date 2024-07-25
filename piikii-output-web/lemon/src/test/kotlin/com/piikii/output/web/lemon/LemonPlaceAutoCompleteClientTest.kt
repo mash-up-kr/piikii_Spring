6fe442010e5e236061e157410832d8230069a441
KimDoubleB <tree9295@gmail.com>
2024-07-25 21:16:22 +0900

    feat: added lemon client test

---
+++ piikii-output-web/lemon/src/test/kotlin/com/piikii/output/web/lemon/LemonPlaceAutoCompleteClientTest.kt
@@ -0,0 +1,25 @@
+package com.piikii.output.web.lemon
+
+import com.piikii.output.web.lemon.adapter.LemonPlaceAutoCompleteClient
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+
+@Disabled
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = [TestConfiguration::class])
+class LemonPlaceAutoCompleteClientTest {
+    @Autowired
+    lateinit var lemonPlaceAutoCompleteClient: LemonPlaceAutoCompleteClient
+
+    @Test
+    fun getAutoCompletedPlaceTest() {
+        val url = "URL을 입력해주세요"
+        val originPlace = lemonPlaceAutoCompleteClient.getAutoCompletedPlace(url)
+        println("originPlace = $originPlace")
+    }
+}
---
+++ piikii-output-web/lemon/src/test/kotlin/com/piikii/output/web/lemon/TestConfiguration.kt
@@ -0,0 +1,10 @@
+package com.piikii.output.web.lemon
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.piikii.output.web.lemon")
+class TestConfiguration
