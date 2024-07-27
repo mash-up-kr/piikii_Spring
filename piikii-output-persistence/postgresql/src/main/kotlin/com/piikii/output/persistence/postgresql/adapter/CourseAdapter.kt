1c0606bf3a48f144f1bb0d6cb8ef70fbe5047ac3
thguss <thgus345@naver.com>
2024-07-27 14:58:28 +0900

    delete: delete course entity

--- piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/CourseAdapter.kt
+++ piikii-output-persistence/postgresql/src/main/kotlin/com/piikii/output/persistence/postgresql/adapter/CourseAdapter.kt
@@ -1,15 +1,13 @@
 package com.piikii.output.persistence.postgresql.adapter
 
 import com.piikii.application.port.output.persistence.CourseQueryPort
-import com.piikii.output.persistence.postgresql.persistence.repository.CourseRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-class CourseAdapter(
-    private val courseRepository: CourseRepository,
-) : CourseQueryPort {
+class CourseAdapter() : CourseQueryPort {
     override fun isCourseExist(roomUid: UUID): Boolean {
-        return courseRepository.existsByroomUid(roomUid)
+        // TODO: implement
+        return false
     }
 }
