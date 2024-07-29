9e8de656246b715c8f0c0580b9a94297981ecf30
Diger <kitten.diger@gmail.com>
2024-07-29 09:19:53 +0900

    chore: CORS Allow

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/web/config/WebConfig.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/web/config/WebConfig.kt
@@ -9,7 +9,7 @@ class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600)
