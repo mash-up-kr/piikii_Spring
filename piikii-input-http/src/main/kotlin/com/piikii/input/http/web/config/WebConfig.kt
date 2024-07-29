b6374e47f45fbfb2bcc3f6b901767e4354c6e333
Diger <kitten.diger@gmail.com>
2024-07-29 09:42:19 +0900

    fix: CORS credentials

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/web/config/WebConfig.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/web/config/WebConfig.kt
@@ -11,7 +11,7 @@ class WebConfig : WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true)
+            .allowCredentials(false)
             .maxAge(3600)
     }
 }
