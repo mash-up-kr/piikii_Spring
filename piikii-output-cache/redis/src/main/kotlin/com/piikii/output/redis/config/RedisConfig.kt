5bfff5f23302efd960d68eda5f5997b29fbbf806
thguss <thgus345@naver.com>
2024-08-28 01:07:56 +0900

    fix: updated custom objectMapper to default

--- piikii-output-cache/redis/src/main/kotlin/com/piikii/output/redis/config/RedisConfig.kt
+++ piikii-output-cache/redis/src/main/kotlin/com/piikii/output/redis/config/RedisConfig.kt
@@ -1,10 +1,6 @@
 package com.piikii.output.redis.config
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.json.JsonMapper
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.CacheManager
@@ -30,16 +26,6 @@ import java.time.Duration
 @EnableCaching
 @EnableConfigurationProperties(RedisProperties::class)
 class RedisConfig {
-    @Bean
-    fun objectMapper(): ObjectMapper {
-        return JsonMapper.builder()
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
-            .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            .configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true)
-            .findAndAddModules()
-            .build()
-    }
 
     @Bean
     fun lettuceConnectionFactory(redisProperties: RedisProperties): LettuceConnectionFactory {
@@ -59,11 +45,11 @@ class RedisConfig {
     }
 
     @Bean
-    fun redisTemplate(redisProperties: RedisProperties): RedisTemplate<String, Any> {
+    fun redisTemplate(redisProperties: RedisProperties, objectMapper: ObjectMapper): RedisTemplate<String, Any> {
         val redisTemplate = RedisTemplate<String, Any>()
         redisTemplate.connectionFactory = lettuceConnectionFactory(redisProperties)
         redisTemplate.keySerializer = StringRedisSerializer()
-        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper())
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
         return redisTemplate
     }
 
