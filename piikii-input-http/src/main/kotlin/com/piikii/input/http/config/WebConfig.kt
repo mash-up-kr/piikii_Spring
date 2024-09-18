2b306dc2676b8d1f25f56e4fc254c1f134680c7a
KimDoubleB <tree9295@gmail.com>
2024-09-19 01:13:04 +0900

    refactor: update packages
    - web.advice -> controller.advice
    - web.config -> config

--- piikii-input-http/src/main/kotlin/com/piikii/input/http/config/WebConfig.kt
+++ piikii-input-http/src/main/kotlin/com/piikii/input/http/config/WebConfig.kt
@@ -1,4 +1,4 @@
-package com.piikii.input.http.web.config
+package com.piikii.input.http.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
