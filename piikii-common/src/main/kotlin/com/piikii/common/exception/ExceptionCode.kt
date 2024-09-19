ade5a0d4655087001e4851d49fe3117f4a6ece8a
KimDoubleB <tree9295@gmail.com>
2024-09-19 23:47:36 +0900

    feat: added ExceptionCode for duplicated request/timeout request

--- piikii-common/src/main/kotlin/com/piikii/common/exception/ExceptionCode.kt
+++ piikii-common/src/main/kotlin/com/piikii/common/exception/ExceptionCode.kt
@@ -8,6 +8,7 @@ enum class ExceptionCode(
     ILLEGAL_ARGUMENT_EXCEPTION(400, "요청 값이 올바르지 않습니다."),
     VOTE_PLACE_ID_INVALID(400, "투표 항목 데이터(Place Id)이 올바르지 않습니다."),
     NOT_SUPPORT_AUTO_COMPLETE_URL(400, "자동입력 지원을 하지 않는 주소형식 입니다."),
+    DUPLICATED_REQUEST(400, "같은 리소스에 대한 중복요청입니다."),
 
     UNAUTHORIZED(401, "인증된 토큰으로부터의 요청이 아닙니다."),
     ROOM_PASSWORD_INVALID(401, "방 패스워드가 틀립니다."),
@@ -19,4 +20,5 @@ enum class ExceptionCode(
     SECRET_MANAGER_CONFIG_NOT_SET(500, "시크릿 매니저 설정 값이 입력되지 않았습니다."),
     URL_PROCESS_ERROR(500, "URL에 해당하는 장소의 정보를 불러오는 중 예기치 못한 오류가 발생했습니다."),
     ROUTE_PROCESS_ERROR(500, "경로 거리 정보를 불러오는 중 예기치 못한 오류가 발생했습니다."),
+    REQUEST_TIMEOUT(500, "요청처리에 Timeout이 발생하였습니다."),
 }
